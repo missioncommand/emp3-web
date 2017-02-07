6f5b8a1213877b8f75036fafb93b266156db20c4
Jay Barra <jay.f.barra.ctr@mail.mil>
2017-02-07 15:12:19 -0500

    fixed missing minified CSS in release validation tool

--- grunt/cssmin.js
+++ grunt/cssmin.js
@@ -12,7 +12,19 @@ module.exports = {
         "src/mapengine/leaflet/css/MarkerCluster.Default.css",
         "src/mapengine/leaflet/css/leaflet-eng.css"
       ],
+      'dist/emp3/emp3-leaflet/emp3-leaflet.min.css': [
+        "src/mapengine/leaflet/css/leaflet.css",
+        "src/mapengine/leaflet/css/MarkerCluster.css",
+        "src/mapengine/leaflet/css/MarkerCluster.Default.css",
+        "src/mapengine/leaflet/css/leaflet-eng.css"
+      ],
       'dist/emp3-map/emp3-cesium/emp3-cesium.min.css': [
+        "src/mapengine/cesium/js/lib/cesium/Cesium.css",
+        "src/mapengine/cesium/js/lib/cesium/Widgets/widgets.css",
+        "src/mapengine/cesium/js/lib/cesium/editors/drawHelper/DrawHelper.css",
+        "src/mapengine/cesium/js/lib/cesium/Widgets/BaseLayerPicker/BaseLayerPicker.css"
+      ],
+      'dist/emp3/emp3-cesium/emp3-cesium.min.css': [
         "src/mapengine/cesium/js/lib/cesium/Cesium.css",
         "src/mapengine/cesium/js/lib/cesium/Widgets/widgets.css",
         "src/mapengine/cesium/js/lib/cesium/editors/drawHelper/DrawHelper.css",
