8f29c44b6d2d533a0949702e5396bd3785a06325
Stephen Pinizzotto <stephen.pinizzotto@shonborn-becker.com>
2017-02-09 17:27:04 -0500

    Changed regular expression to catch data images, as well as moved the colon to the individual protocols in the the regex

--- src/sdk/core/emp.helpers.js
+++ src/sdk/core/emp.helpers.js
@@ -640,7 +640,7 @@ emp.helpers.relToAbs = function (url) {
   /* Only accept commonly trusted protocols:
    * Only data-image URLs are accepted, Exotic flavours (escaped slash,
    * html-entitied characters) are not supported to keep the function fast */
-  if (/^(https?|file|ftps?|mailto|javascript|data:image\/):/i.test(url))
+  if (/^(https?:|file:|ftps?:|mailto:|javascript:|data:image\/[^;]{2,9})/i.test(url))
     return url; //Url is already absolute
 
   var base_url = location.href.match(/^(.+)\/?(?:#.+)?$/)[0] + "/";
