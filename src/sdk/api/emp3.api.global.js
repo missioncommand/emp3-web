1cc4fbe58aa90ef5a004b4e9f47c539b3cdb6752
Stephen Pinizzotto <stephen.pinizzotto@shonborn-becker.com>
2017-02-15 14:11:22 -0500

    Added version into release.

--- src/sdk/api/emp3.api.global.js
+++ src/sdk/api/emp3.api.global.js
@@ -15,6 +15,8 @@ if (!window.emp3.api) {
  * and other map service related requests.
  */
 emp3.api.global = emp3.api.global || {
+
+    version: "2.0.0",
     /**
      * @name emp3.api.global#configuration
      */
