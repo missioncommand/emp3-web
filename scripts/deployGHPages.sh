dcea4b63467845b4ae66c5d5083d308c5ce83dde
Jay Barra <jay.f.barra.ctr@mail.mil>
2017-02-15 14:06:10 -0500

    updated the deploy script, need to test on something live first #90

--- scripts/deployGHPages.sh
+++ scripts/deployGHPages.sh
@@ -2,13 +2,23 @@
 
 set -o errexit
 
-# config
+# Config the git user
 git config --global user.email "nobody@nobody.org"
 git config --global user.name "Travis CI"
 
-# deploy
-cd dist/devguide
-git init
-git add .
-git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GH_PAGES_REPO}.git" master:gh-pages > /dev/null 2>&1
+# Clone existing gh-pages to dist
+cd dist
+rm -rf ghpages
+git clone "https://github.com/${GH_PAGES_REPO}.git" ghpages
+
+## Remove the old devguide and replace it
+rm -rf ghpages/docs/emp/web
+cp -a devguide/ ghpages/docs/emp/web
+
+# Make the changes in git
+cd ghpages
+git add -A docs/emp/web
+git commit -m "Deploying updated GitHub Pages"
+
+# Deploy
+#git push --quiet "https://${GITHUB_TOKEN}@$github.com/${GH_PAGES_REPO}.git" master:gh-pages > /dev/null 2>&1
