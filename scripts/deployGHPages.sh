804264484fda41d7f3b4f8f428339f311a62d5ca
unknown <nobody@nobody.org>
2017-02-21 10:31:03 -0500

    still more debugging, trying to get echo to work

--- scripts/deployGHPages.sh
+++ scripts/deployGHPages.sh
@@ -13,11 +13,13 @@ rm -rf ghpages
 git clone "https://github.com/${GH_PAGES_REPO}.git" ghpages
 
 ## Remove the old devguide and replace it
-echo "Replacing devguide contents"
+echo ""
+echo "Updating devguide contents"
 rm -rf ghpages/docs/emp/web
 cp -a devguide/ ghpages/docs/emp/web
 
 # Make the changes in git
+echo ""
 echo "Applying changes"
 cd ghpages
 git add -A docs/emp/web
@@ -25,5 +27,6 @@ git commit -m "Deploying updated GitHub Pages"
 git status
 
 # Deploy
+echo ""
 echo "Pushing changes"
 #git push --quiet "https://${GITHUB_TOKEN}@$github.com/${GH_PAGES_REPO}.git" master:gh-pages > /dev/null 2>&1
