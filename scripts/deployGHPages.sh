fcaf4b3bcfbe0f880fa9a6a13c75d32cec7a88cb
Jay Barra <jay.f.barra.ctr@mail.mil>
2017-02-21 13:33:33 -0500

    trying to get travis script deployment configured

--- scripts/deployGHPages.sh
+++ scripts/deployGHPages.sh
@@ -11,15 +11,12 @@ cd dist
 rm -rf ghpages
 git clone "https://github.com/${GH_PAGES_REPO}.git" ghpages
 
-exit 2
-
 ## Remove the old devguide and replace it
 echo ""
 echo "Updating devguide contents"
 rm -rf ghpages/docs/emp/web
 cp -a devguide/ ghpages/docs/emp/web
 
-exit 3
 # Make the changes in git
 echo ""
 echo "Applying changes"
@@ -29,11 +26,7 @@ git commit -m "Deploying updated GitHub Pages"
 
 git remote add upstream "https://${GITHUB_API_KEY}@github.com/${GH_PAGES_REPO}.git"
 
-exit 4
-
 # Deploy
 echo ""
 echo "Pushing changes"
 git push -q upstream HEAD:gh-pages
-
-exit 5
