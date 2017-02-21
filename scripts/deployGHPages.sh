90ddde814fbe50ebf056a8ef3361bc25d4ac244a
Jay Barra <jay.f.barra.ctr@mail.mil>
2017-02-21 10:13:47 -0500

    more debugging

--- scripts/deployGHPages.sh
+++ scripts/deployGHPages.sh
@@ -7,19 +7,23 @@ git config --global user.email "nobody@nobody.org"
 git config --global user.name "${GH_PAGES_USER}"
 
 # Clone existing gh-pages to dist
+echo "Cloning Existing Repo"
 cd dist
 rm -rf ghpages
 git clone "https://github.com/${GH_PAGES_REPO}.git" ghpages
 
 ## Remove the old devguide and replace it
+echo "Replacing devguide contents"
 rm -rf ghpages/docs/emp/web
 cp -a devguide/ ghpages/docs/emp/web
 
 # Make the changes in git
+echo "Applying changes"
 cd ghpages
 git add -A docs/emp/web
 git commit -m "Deploying updated GitHub Pages"
 git status
 
 # Deploy
-git push --quiet "https://${GITHUB_TOKEN}@$github.com/${GH_PAGES_REPO}.git" master:gh-pages > /dev/null 2>&1
+echo "Pushing changes"
+#git push --quiet "https://${GITHUB_TOKEN}@$github.com/${GH_PAGES_REPO}.git" master:gh-pages > /dev/null 2>&1
