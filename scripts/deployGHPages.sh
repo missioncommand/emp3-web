cb81897da584964ddea642dbd90b64acdd9a1f66
Jay Barra <jay.f.barra.ctr@mail.mil>
2017-02-22 09:51:26 -0500

    updating deployment script

--- scripts/deployGHPages.sh
+++ scripts/deployGHPages.sh
@@ -1,33 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-set -o errexit -o nounset
+echo $@
 
-# Config the git user
-git config --global user.name "missioncommandbot"
-git config --global user.email "missioncommandbot@missioncommandbot.org"
-
-# Clone existing gh-pages to dist
-echo "Cloning Existing Repo"
-cd dist
-rm -rf ghpages
-git clone "https://github.com/${GH_PAGES_REPO}.git" ghpages
-
-## Remove the old devguide and replace it
-echo ""
-echo "Updating devguide contents"
-rm -rf ghpages/docs/emp/web
-cp -a devguide/ ghpages/docs/emp/web
-
-# Make the changes in git
-echo ""
-echo "Applying changes"
-cd ghpages
-git add -A docs/emp/web
-git commit -m "Deploying updated GitHub Pages"
-
-git remote add upstream "https://${GITHUB_API_KEY}@github.com/${GH_PAGES_REPO}.git"
-
-# Deploy
-echo ""
-echo "Pushing changes"
-git push -q upstream HEAD:gh-pages
+#
+#set -o errexit -o nounset
+#
+## Config the git user
+#git config --global user.name "missioncommandbot"
+#git config --global user.email "missioncommandbot@missioncommandbot.org"
+#
+## Clone existing gh-pages to dist
+#echo "Cloning Existing Repo"
+#cd dist
+#rm -rf ghpages
+#git clone "https://github.com/${GH_PAGES_REPO}.git" ghpages
+#
+### Remove the old devguide and replace it
+#echo ""
+#echo "Updating devguide contents"
+#rm -rf ghpages/docs/emp/web
+#cp -a devguide/ ghpages/docs/emp/web
+#
+## Make the changes in git
+#echo ""
+#echo "Applying changes"
+#cd ghpages
+#git add -A docs/emp/web
+#git commit -m "Deploying updated GitHub Pages"
+#
+#git remote add upstream "https://${GITHUB_API_KEY}@github.com/${GH_PAGES_REPO}.git"
+#
+## Deploy
+#echo ""
+#echo "Pushing changes"
+#git push -q upstream HEAD:gh-pages
