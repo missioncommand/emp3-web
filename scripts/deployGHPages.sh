1f8a8622878bd2541110d40c67e3b693185c1520
Jay Barra <jay.f.barra.ctr@mail.mil>
2017-02-23 14:32:35 -0500

    updated script

--- scripts/deployGHPages.sh
+++ scripts/deployGHPages.sh
@@ -1,44 +1,33 @@
 #!/bin/bash
 
-echo "kiss kiss"
-echo '$0 = ' $0
-echo '$1 = ' $1
-echo 'KEY = ' $GITHUB_API_KEY
-echo 'REPO = ' $GH_PAGES_REPO
-echo 'THE_COW_SAYS' $THE_COW_SAYS
-echo 'THE_DOG_SAYS' $THE_DOG_SAYS
-echo "bang bang"
-
 set -o errexit -o nounset
-#
-## Config the git user
-#git config --global user.name "missioncommandbot"
-#git config --global user.email "missioncommandbot@missioncommandbot.org"
-#
-## Clone existing gh-pages to dist
-#echo "Cloning Existing Repo"
-#cd dist
-#rm -rf ghpages
-#git clone "https://github.com/${GH_PAGES_REPO}.git" ghpages
-#
-### Remove the old devguide and replace it
-#echo ""
-#echo "Updating devguide contents"
-#rm -rf ghpages/docs/emp/web
-#cp -a devguide/ ghpages/docs/emp/web
-#
-## Make the changes in git
-#echo ""
-#echo "Applying changes"
-#cd ghpages
-#git add -A docs/emp/web
-#git commit -m "Deploying updated GitHub Pages"
-#
-#git remote add upstream "https://${GITHUB_API_KEY}@github.com/${GH_PAGES_REPO}.git"
-#
-## Deploy
-#echo ""
-#echo "Pushing changes"
-#git push -q upstream HEAD:gh-pages
 
-echo "this is where I would commit things, if I had any"
+# Config the git user
+git config --global user.name "missioncommandbot"
+git config --global user.email "missioncommandbot@missioncommandbot.org"
+
+# Clone existing gh-pages to dist
+echo "Cloning Existing Repo"
+cd dist
+rm -rf ghpages
+git clone "https://github.com/${GH_PAGES_REPO}.git" ghpages
+
+## Remove the old devguide and replace it
+echo ""
+echo "Updating devguide contents"
+rm -rf ghpages/docs/emp/web
+cp -a devguide/ ghpages/docs/emp/web
+
+# Make the changes in git
+echo ""
+echo "Applying changes"
+cd ghpages
+git add -A docs/emp/web
+git commit -m "Deploying updated GitHub Pages"
+
+git remote add upstream "https://${MCIO_GITHUB_API_KEY}@github.com/${GH_PAGES_REPO}.git"
+
+# Deploy
+echo ""
+echo "Pushing changes"
+git push -q upstream HEAD:gh-pages
