1adf1bc2bf8a291be957caa3b0ccf5231e6ab9eb
Jay Barra <jay.f.barra.ctr@mail.mil>
2017-03-24 16:51:47 -0400

    moved mouse event handlers to pointer, reduces duplicate handling of events and improves performance in WorldWind

--- src/mapengine/worldwind/controllers/eventHandlers/EMPWorldWind.eventHandlers.pointer.js
+++ src/mapengine/worldwind/controllers/eventHandlers/EMPWorldWind.eventHandlers.pointer.js
@@ -10,9 +10,6 @@ EMPWorldWind.eventHandlers = EMPWorldWind.eventHandlers || {};
  * Pointer events are currently aliases for their mouseevent counterparts
  *
  * @namespace
- * @borrows EMPWorldWind.eventHandlers.mouse.mousedown as pointerdown
- * @borrows EMPWorldWind.eventHandlers.mouse.mouseup as pointerup
- * @borrows EMPWorldWind.eventHandlers.mouse.mousemove as pointermove
  */
 EMPWorldWind.eventHandlers.pointer = {
   /**
@@ -20,15 +17,98 @@ EMPWorldWind.eventHandlers.pointer = {
    * @see EMPWorldWind.eventHandlers.mouse.mousedown
    * @this EMPWorldWind.map
    */
-  pointerdown: EMPWorldWind.eventHandlers.mouse.mousedown,
+  pointerdown: function(event) {
+    var mousedownEvent = EMPWorldWind.utils.getEventCoordinates.call(this, event);
+
+    mousedownEvent.type = emp.typeLibrary.Pointer.EventType.MOUSEDOWN;
+    EMPWorldWind.eventHandlers.extractFeatureFromEvent.call(this, event, mousedownEvent);
+
+    this.empMapInstance.eventing.Pointer(mousedownEvent);
+  },
   /**
    * @see EMPWorldWind.eventHandlers.mouse.mouseup
    * @this EMPWorldWind.map
    */
-  pointerup: EMPWorldWind.eventHandlers.mouse.mouseup,
+  pointerup: function(event) {
+    var mouseupEvent = EMPWorldWind.utils.getEventCoordinates.call(this, event);
+    mouseupEvent.type = emp.typeLibrary.Pointer.EventType.MOUSEUP;
+    // TODO see if features are needed on mouseup
+    // EMPWorldWind.eventHandlers.extractFeatureFromEvent.call(this, event, mouseupEvent);
+
+    if (this.state.dragging) {
+      this.state.dragging = false;
+      EMPWorldWind.eventHandlers.notifyViewChange.call(this, emp3.api.enums.CameraEventEnum.CAMERA_MOTION_STOPPED);
+    }
+
+    this.state.autoPanning = EMPWorldWind.constants.NO_PANNING;
+    this.empMapInstance.eventing.Pointer(mouseupEvent);
+  },
   /**
    * @see EMPWorldWind.eventHandlers.mouse.mousemove
    * @this EMPWorldWind.map
    */
-  pointermove: EMPWorldWind.eventHandlers.mouse.mousemove
+  pointermove: function(event) {
+    var coords = EMPWorldWind.utils.getEventCoordinates.call(this, event);
+    coords.type = emp.typeLibrary.Pointer.EventType.MOVE;
+
+    // TODO speed this up somehow
+    EMPWorldWind.eventHandlers.extractFeatureFromEvent.call(this, event, coords);
+    if (coords.lat !== undefined) {
+      this.empMapInstance.eventing.Pointer(coords);
+    }
+
+    var element, elementBounds,
+      smartAreaBuffer = 0.05,
+      pan = {
+        up: false,
+        down: false,
+        left: false,
+        right: false
+      };
+
+    switch (event.buttons) {
+      case 1: // Left button, we're moving the map
+      case 2: // Right button, we're tilting/rotating the map
+        switch (this.state.lockState) {
+          case emp3.api.enums.MapMotionLockEnum.NO_MOTION:
+          case emp3.api.enums.MapMotionLockEnum.NO_PAN:
+          case emp3.api.enums.MapMotionLockEnum.NO_ZOOM_NO_PAN:
+            this.state.dragging = true;
+            event.preventDefault();
+            break;
+          case emp3.api.enums.MapMotionLockEnum.SMART_MOTION:
+            event.preventDefault();
+
+            element = event.srcElement || event.originalTarget;
+            elementBounds = element.getBoundingClientRect();
+
+            // Pan left or right
+            pan.left = event.offsetX < elementBounds.width * smartAreaBuffer;
+            pan.right = event.offsetX > elementBounds.width - (elementBounds.width * smartAreaBuffer);
+
+            // Pan up or down
+            pan.up = event.offsetY < elementBounds.height * smartAreaBuffer;
+            pan.down = event.offsetY > elementBounds.height - (elementBounds.height * smartAreaBuffer);
+
+            if (pan.up || pan.down || pan.left || pan.right) {
+              this.state.autoPanning = pan;
+              this.spinGlobe();
+            } else {
+              this.state.autoPanning = EMPWorldWind.constants.NO_PANNING;
+            }
+            break;
+          case emp3.api.enums.MapMotionLockEnum.UNLOCKED:
+          default:
+            EMPWorldWind.eventHandlers.notifyViewChange.call(this);
+        }
+        break;
+      case 4: // Wheel/middle button
+      case 8: // 4th button (back)
+      case 16: // 5th button (forward)
+      default:
+      // No actions
+    }
+
+    this.state.lastInteractionEvent = event;
+  }
 };
