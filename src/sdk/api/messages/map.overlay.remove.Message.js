72c853d1bea2d6d1d72c1109ff55a6cae4f565e2
Jay Barra <jay.f.barra.ctr@mail.mil>
2017-04-03 09:58:46 -0400

    encapsulating the message classes for safety

--- src/sdk/api/messages/map.overlay.remove.Message.js
+++ src/sdk/api/messages/map.overlay.remove.Message.js
@@ -1,34 +1,38 @@
-/**
- * @extends emp3.api.Message
- *
- * @ignore
- * @class
- * @param {object} message
- * @param {emp3.api.Overlay[]} message.overlays
- * @param {string} message.parentId
- * @param {object} callInfo Unused for this message
- * @param {string} transactionId
- */
-function MapOverlayRemoveMessage(message, callInfo, transactionId) {
-  emp3.api.Message.call(this, emp3.api.enums.channel.removeOverlay, transactionId);
+(function() {
+  "use strict";
 
-  this.payload = [];
-  emp.util.each(message.overlays, function(overlay) {
-    this.payload.push({
-      overlayId: overlay.geoId,
-      parentId: message.parentId,
-      messageId: transactionId
-    });
-  }.bind(this));
-}
+  /**
+   * @extends emp3.api.Message
+   *
+   * @ignore
+   * @class
+   * @param {object} message
+   * @param {emp3.api.Overlay[]} message.overlays
+   * @param {string} message.parentId
+   * @param {object} callInfo Unused for this message
+   * @param {string} transactionId
+   */
+  function MapOverlayRemoveMessage(message, callInfo, transactionId) {
+    emp3.api.Message.call(this, emp3.api.enums.channel.removeOverlay, transactionId);
 
-// Extend emp3.api.Message
-MapOverlayRemoveMessage.prototype = Object.create(emp3.api.Message.prototype);
+    this.payload = [];
+    emp.util.each(message.overlays, function(overlay) {
+      this.payload.push({
+        overlayId: overlay.geoId,
+        parentId: message.parentId,
+        messageId: transactionId
+      });
+    }.bind(this));
+  }
 
-//======================================================================================================================
-var channels = [
-  emp3.api.enums.channel.removeOverlay
-];
+  // Extend emp3.api.Message
+  MapOverlayRemoveMessage.prototype = Object.create(emp3.api.Message.prototype);
 
-// Register with the MessageFactory
-emp3.api.MessageFactory.registerMessage(channels, MapOverlayRemoveMessage);
+  //====================================================================================================================
+  var channels = [
+    emp3.api.enums.channel.removeOverlay
+  ];
+
+  // Register with the MessageFactory
+  emp3.api.MessageFactory.registerMessage(channels, MapOverlayRemoveMessage);
+}());
--- src/sdk/api/messages/map.swap.Message.js
+++ src/sdk/api/messages/map.swap.Message.js
@@ -1,33 +1,37 @@
-/**
- * @extends emp3.api.Message
- *
- * Sends the message required to swap a map engine.
- *
- * @ignore
- * @class
- * @param {object} message
- * @param {object} message.engine
- * @param {object} callInfo
- * @param {string} callInfo.mapId The geoId of the map to be swapped
- * @param {string} transactionId
- */
-function MapSwapMessage(message, callInfo, transactionId) {
-  emp3.api.Message.call(this, emp3.api.enums.channel.swap, transactionId);
+(function() {
+  "use strict";
 
-  this.payload = {
-    mapId: callInfo.mapId,
-    engine: message.engine,
-    messageId: transactionId
-  };
-}
+  /**
+   * @extends emp3.api.Message
+   *
+   * Sends the message required to swap a map engine.
+   *
+   * @ignore
+   * @class
+   * @param {object} message
+   * @param {object} message.engine
+   * @param {object} callInfo
+   * @param {string} callInfo.mapId The geoId of the map to be swapped
+   * @param {string} transactionId
+   */
+  function MapSwapMessage(message, callInfo, transactionId) {
+    emp3.api.Message.call(this, emp3.api.enums.channel.swap, transactionId);
 
-// Extend emp3.api.Message
-MapSwapMessage.prototype = Object.create(emp3.api.Message.prototype);
+    this.payload = {
+      mapId: callInfo.mapId,
+      engine: message.engine,
+      messageId: transactionId
+    };
+  }
 
-//======================================================================================================================
-var channels = [
-  emp3.api.enums.channel.swap
-];
+  // Extend emp3.api.Message
+  MapSwapMessage.prototype = Object.create(emp3.api.Message.prototype);
 
-// Register with the MessageFactory
-emp3.api.MessageFactory.registerMessage(channels, MapSwapMessage);
+  //====================================================================================================================
+  var channels = [
+    emp3.api.enums.channel.swap
+  ];
+
+  // Register with the MessageFactory
+  emp3.api.MessageFactory.registerMessage(channels, MapSwapMessage);
+}());
