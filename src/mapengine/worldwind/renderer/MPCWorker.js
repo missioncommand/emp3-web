9031410da9c1636c501ab17c2cfd12786ba9c961
alberto.acevedo <vascoace@aol.com>
2017-04-06 08:35:44 -0400

    test commit and push in atom.

--- src/mapengine/worldwind/renderer/MPCWorker.js
+++ src/mapengine/worldwind/renderer/MPCWorker.js
@@ -66,6 +66,7 @@ importScripts('savm-bc.min.js');//for strictly KML, GeoJSON and SVG(with hatch l
 //importScripts('worldwind.js');
 //importScripts('workerImports.js');
 // test atom push. broiken again.
+//test
 
 var rendererMP = sec.web.renderer.SECWebRenderer;
 
