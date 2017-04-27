673552eb661323976e1cfccbbc5794435da56551
Stephen Pinizzotto <stephen.pinizzotto@shonborn-becker.com>
2017-04-27 17:31:47 -0400

    Added editor's addControlPoint method.

--- src/sdk/core/emp.editingManager.js
+++ src/sdk/core/emp.editingManager.js
@@ -138,6 +138,12 @@ emp.editingManager = function(args) {
         feature: feature,
         mapInstance: args.mapInstance
       });
+    } else {
+      // This is an ellipse.  It is a point with a semi minor and semi major..
+      activeEditor = new emp.editors.EditorBase({
+        feature: feature,
+        mapInstance: args.mapInstance
+      });
     }
 
     return activeEditor;
@@ -194,9 +200,6 @@ emp.editingManager = function(args) {
      */
     edit: function(transaction) {
 
-      var symbol = false,
-        drawCategory;
-
       // indicate that this isn't a draw.
       drawing = false;
 
@@ -243,100 +246,7 @@ emp.editingManager = function(args) {
         feature.overlayId = emp.constant.parentIds.ALL_PARENTS;
       }
 
-      // Determine if this is a MIL Symbol.  The mil symbol categories can greatly
-      // vary, so we need to know the symbol code and standard for this.
-      if (feature.format === emp3.api.enums.FeatureTypeEnum.GEO_MIL_SYMBOL) {
-        symbol = true;
-
-        drawCategory = emp.util.getDrawCategory(feature);
-      }
-
-      // Determine the type of editor needed.
-      // create the editor for the appropriate item being edited.
-      if (feature.format === emp3.api.enums.FeatureTypeEnum.GEO_POINT ||
-        (symbol && drawCategory === armyc2.c2sd.renderer.utilities.SymbolDefTable.DRAW_CATEGORY_POINT)) {
-        // This is a single point item.  Single point items follow the same rules
-        // regardless if it is MIL-STD or not.
-        activeEditor = new emp.editors.Point({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else if (feature.format === emp3.api.enums.FeatureTypeEnum.GEO_PATH ||
-        (symbol && drawCategory === armyc2.c2sd.renderer.utilities.SymbolDefTable.DRAW_CATEGORY_LINE)) {
-        // This is a path.  These are items that follow the rules of a multipoint
-        // line.  It could be MIL-STD or not.
-        activeEditor = new emp.editors.Path({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else if (feature.format === emp3.api.enums.FeatureTypeEnum.GEO_POLYGON) {
-        // This is a polygon.   MIL-STD polygons are handled slightly different
-        // so there is a separate editor for those.
-        activeEditor = new emp.editors.Polygon({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else if (symbol && drawCategory === armyc2.c2sd.renderer.utilities.SymbolDefTable.DRAW_CATEGORY_POLYGON) {
-        // This is a MIL-STD polygon.  It uses a GEOJSON linestring to represent
-        // itself.  It is stored slightly different than the regular polygon.
-        activeEditor = new emp.editors.MilStdPolygon({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else if (feature.format === emp3.api.enums.FeatureTypeEnum.GEO_CIRCLE ||
-        (symbol && drawCategory === armyc2.c2sd.renderer.utilities.SymbolDefTable.DRAW_CATEGORY_CIRCULAR_PARAMETERED_AUTOSHAPE)) {
-        // This is a circle defined by a point and radius.  It uses a GEOJSON point and
-        // a distance in meters to represent
-        // itself.
-        activeEditor = new emp.editors.Circle({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else if (feature.format === emp3.api.enums.FeatureTypeEnum.GEO_ELLIPSE) {
-        // This is a ellipse defined by a point and radius.  It uses a GEOJSON point and
-        // a distance in meters to represent
-        // itself.
-        activeEditor = new emp.editors.Ellipse({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else if (feature.format === emp3.api.enums.FeatureTypeEnum.GEO_RECTANGLE ||
-        (symbol && drawCategory === armyc2.c2sd.renderer.utilities.SymbolDefTable.DRAW_CATEGORY_RECTANGULAR_PARAMETERED_AUTOSHAPE)) {
-        // This is a rectangle defined by a point, width, height and azimuth.
-        activeEditor = new emp.editors.Rectangle({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else if (feature.format == emp3.api.enums.FeatureTypeEnum.GEO_SQUARE) {
-        // This is a square.
-        // so there is a separate editor for those.
-        activeEditor = new emp.editors.Square({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else if (symbol && drawCategory === armyc2.c2sd.renderer.utilities.SymbolDefTable.DRAW_CATEGORY_ROUTE) {
-        // This is an axis of advance defined by a line.  The last point of the line in relation to the first point
-        // decides the width of the axis of advance.
-        activeEditor = new emp.editors.AxisOfAdvance({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
-      else {
-        // create the editor for the appropriate item being edited.
-        activeEditor = new emp.editors.EditorBase({
-          feature: feature,
-          mapInstance: args.mapInstance
-        });
-      }
+      activeEditor = getEditor(feature);
 
       // have the editor create any editing features on the map.
       activeEditor.addControlPoints();
