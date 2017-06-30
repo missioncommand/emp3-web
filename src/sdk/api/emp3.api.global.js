e4a5b054e1a6134a7d8a174564e96b74bbb2b26d
alberto.acevedo <vascoace@aol.com>
2017-06-30 11:12:14 -0400

    -upgraded api global to 2.8.0

--- src/sdk/api/emp3.api.global.js
+++ src/sdk/api/emp3.api.global.js
@@ -16,7 +16,7 @@ if (!window.emp3.api) {
  */
 emp3.api.global = emp3.api.global || {
 
-    version: "2.8.0_dev",
+    version: "2.8.0",
     /**
      * @name emp3.api.global#configuration
      */
