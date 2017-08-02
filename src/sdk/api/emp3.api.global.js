3a443bdd98cef93458f7091c73a36afd8e52f264
alberto.acevedo <vascoace@aol.com>
2017-08-02 12:14:38 -0400

    set global version to 2.11.0_dev

--- src/sdk/api/emp3.api.global.js
+++ src/sdk/api/emp3.api.global.js
@@ -16,7 +16,7 @@ if (!window.emp3.api) {
  */
 emp3.api.global = emp3.api.global || {
 
-    version: "2.10.0",
+    version: "2.11.0_dev", 
     /**
      * @name emp3.api.global#configuration
      */
